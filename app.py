cd235e3835c6ae217e09ff7b35d0d21f0d3187e4
Ubuntu <ubuntu@ip-172-31-5-14.ap-south-1.compute.internal>
2020-09-14 15:15:54 +0000

    Final

--- app.py
+++ app.py
@@ -34,27 +34,34 @@ def api_all():
 
     spark._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
     spark._jsc.hadoopConfiguration().set("com.amazonaws.services.s3.enableV4", "true")
-    spark._jsc.hadoopConfiguration().set("fs.s3a.awsAccessKeyId", str(os.getenv["AWS_ACCESS_KEY_ID"]))
-    spark._jsc.hadoopConfiguration().set("fs.s3a.awsSecretAccessKey", str(os.getenv["AWS_SECRET_ACCESS_KEY"]))
+    spark._jsc.hadoopConfiguration().set("fs.s3a.awsAccessKeyId", str(os.getenv("AWS_ACCESS_KEY_ID")))
+    spark._jsc.hadoopConfiguration().set("fs.s3a.awsSecretAccessKey", str(os.getenv("AWS_SECRET_ACCESS_KEY")))
     sc = spark.sparkContext
 
     sqlContext = SQLContext(sc)
-
+    bucket = "mytest3838"
+    print(bucket)
     if request.method == 'POST':
-        file_key = request.data.get('file_key')
-        schema = StructType([])
+        print(request.json)
+        file_key = request.json.get('file_key')
+        #schema = StructType([])
         file_folder = "/".join(file_key.rsplit("/")[:-1])
-        output_data = sqlContext.createDataFrame(sc.emptyRDD(), schema)
-        for index, val in enumerate(s3.Bucket("layers3parqut").objects.filter(Prefix=file_folder + "/")):
-            if index != 0:
+        #output_data = sqlContext.createDataFrame(sc.emptyRDD(), schema)
+        dff = None
+        for index, val in enumerate(s3.Bucket(bucket).objects.filter(Prefix=file_folder + "/")):
+            if index == 1:
                 if str(val.key).endswith(".parquet"):
-                    print(f's3://layers3parqut/{val.key}')
-                    df1 = sqlContext.read.parquet("s3a://layers3parqut/ubx.dev.mcpidevint1+3+0000622214-snappy.parquet")
-                    # df2 = sqlContext.read.parquet("s3a://layers3parqut/ubx-dev-mcpidevint1+2+0000619666-snappy.parquet")
-                    output_data.union(df1)
-
-        output_data.toPandas().to_parquet(
-            f's3://layers3parqut//{file_folder}/{"_".join(file_key.rsplit("/")[:-1])}_MERGE.parquet')
+                    print(f's3://{bucket}/{val.key}')
+                    df = sqlContext.read.parquet(f"s3a://{bucket}/{val.key}")
+                    dff = df
+            elif index != 0:
+               # df2 = sqlContext.read.parquet("s3a://layers3parqut/ubx-dev-mcpidevint1+2+0000619666-snappy.parquet")
+                    df = sqlContext.read.parquet(f"s3a://{bucket}/{val.key}")
+                    dff=dff.unionAll(df)
+                    #output_data.union(df1)
+        print(dff,"---")
+        dff.toPandas().to_parquet(
+            f's3://{bucket}//{file_folder}/{"_".join(file_key.rsplit("/")[:-1])}_MERGE.parquet')
 
     return jsonify({
         "status": "success"
@@ -62,4 +69,9 @@ def api_all():
 
 
 if __name__ == '__main__':
+   # from gevent.pywsgi import WSGIServer
+    #app.debug = True 
+    #http_server = WSGIServer(('0.0.0.0', 5000), app)
+    #http_server.serve_forever()
+    
     app.run(host='0.0.0.0')
